a0dc3e2fe9a28b9b8746f6a6842aa2cb354b262c
Boris Cool <boris.cool@outlook.be>
2025-01-29 09:09:35 +0100

    dialoog

--- Escape conversatie/Program.cs
+++ Escape conversatie/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string personage1 = "Alice";
+            string personage2 = "Bob";
+
+            string dialoog = $"Alice: \"Hoe gaat het met je?\"\r\n    Bob: \"Goed, dank je! Hoe gaat het met jou?\"\r\nAlice: \"Ook goed, bedankt dat je het vraagt.\"";
+
+            Console.WriteLine(dialoog);
         }
     }
 }
