c8de7da399a9cdd72e416cceee688b3b5e3b9bfc
Boris Cool <boris.cool@outlook.be>
2025-01-29 09:01:28 +0100

    oefening gemaakt

--- Mad Libs/Program.cs
+++ Mad Libs/Program.cs
@@ -4,8 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, boris!");
-            Console.WriteLine("hallo boris");
+            Console.Write("Wat is je naam? ");
+            string naam = Console.ReadLine();
+            Console.Write("Kies een zelfstandig naamwoord: ");
+            string naamwoord = Console.ReadLine();
+            Console.Write("Kies een adjectief: ");
+            string adjectief = Console.ReadLine();
+            Console.Write("Kies een werkwoord: ");
+            string werkwoord = Console.ReadLine();
+
+            Console.WriteLine($"\nOp een dag ging {naam} naar de AP Hogeschool. Hij zag daar een {adjectief} {naamwoord} en vond dat zo grappig dat hij begon te {werkwoord}.");
         }
     }
 }
